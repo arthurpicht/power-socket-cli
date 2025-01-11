171a82a060adee8235d759c46cda4ed0aa261226
Arthur Picht <picht@arthurpicht.de>
2025-01-11 21:26:18 +0100

    dummy power module

--- src/main/java/de/arthurpicht/powerSocketCli/PowerSocketFactory.java
+++ src/main/java/de/arthurpicht/powerSocketCli/PowerSocketFactory.java
@@ -2,22 +2,35 @@ package de.arthurpicht.powerSocketCli;
 
 import de.arthurpicht.powerSocketApi.PowerSocket;
 import de.arthurpicht.powerSocketApi.common.PowerSocketConfig;
+import de.arthurpicht.powerSocketApi.dummy.DummyConfig;
 import de.arthurpicht.powerSocketApi.infratecPM8.InfratecPM8Config;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class PowerSocketFactory {
 
-    public static PowerSocket create() {
-        String deviceId = "infratec-PM8-1";
+//    public static PowerSocket create() {
+//        String deviceId = "infratec-PM8-1";
+//
+//        PowerSocketConfig powerSocketConfig = new InfratecPM8Config(
+//                deviceId,
+//                "192.168.11.5",
+//                "admin",
+//                "geheim"
+//        );
+//
+//        List<PowerSocketConfig> powerSocketConfigList = new ArrayList<>();
+//        powerSocketConfigList.add(powerSocketConfig);
+//
+//        return new PowerSocket(powerSocketConfigList);
+//    }
 
-        PowerSocketConfig powerSocketConfig = new InfratecPM8Config(
-                deviceId,
-                "192.168.11.5",
-                "admin",
-                "geheim"
-        );
+    public static PowerSocket create() {
+        String deviceId = "dummy-power-module";
+
+        PowerSocketConfig powerSocketConfig = new DummyConfig(deviceId, Arrays.asList("out-1", "out-2", "out-3", "out-4"));
 
         List<PowerSocketConfig> powerSocketConfigList = new ArrayList<>();
         powerSocketConfigList.add(powerSocketConfig);
